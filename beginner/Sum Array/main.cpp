2c095bce3d833f4be8461c774b28bcdfbd9ae2e3
rohan <rohanluthra14@gmail.com>
2020-02-26 00:17:51 +0530

    beginner

---
+++ beginner/Sum Array/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            int temp;
+            cin>>temp; 
+            if(i==j || (i+j)==(n-1) || i==0 || j==0 || i==(n-1) || j==(n-1)){
+                sum+=temp;
+            }
+
+        }
+    }
+    
+    // int sum =0;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(i==j || (i+j)==(n-1) || i==0 || j==0 || i==(n-1) || j==(n-1)){
+    //             sum+=a[i][j];
+    //         }    
+    //     }
+    // }
+    
+    cout<<sum;
+	return 0;
+}
